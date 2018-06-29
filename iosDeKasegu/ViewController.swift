44f59b817574a9dc71c82703397898c2188985af
高木広大 <kodai@takagikoudai-no-macbook-pro.local>
2018-06-29 21:53:11 +0900

    Initial Commit

---
+++ iosDeKasegu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iosDeKasegu
+//
+//  Created by 高木広大 on 2018/06/29.
+//  Copyright © 2018年 shonanhiratsuka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
