ef4664283ef9f6e4f6131734fd4b24120751a995
Kodai Takagi <mj5824@icloud.com>
2018-07-01 21:23:56 +0900

    ・BaseViewControllerの追加(UIViewControllerを継承していて、GADBannerViewDelegateに必要な関数、広告追加関数を実装)

---
+++ iosDeKasegu/BaseViewController.swift
@@ -0,0 +1,68 @@
+//
+//  GADBannerManager.swift
+//  iosDeKasegu
+//
+//  Created by 高木広大 on 2018/07/01.
+//  Copyright © 2018年 shonanhiratsuka. All rights reserved.
+//
+
+import Foundation
+import GoogleMobileAds
+
+class BaseViewController: UIViewController, GADBannerViewDelegate {
+    let admob_id = "ca-app-pub-3940256099942544/2934735716"
+    
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: bottomLayoutGuide,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+            ])
+    }
+    /// Tells the delegate an ad request loaded an ad.
+    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
+        print("adViewDidReceiveAd")
+    }
+    
+    /// Tells the delegate an ad request failed.
+    func adView(_ bannerView: GADBannerView,
+                didFailToReceiveAdWithError error: GADRequestError) {
+        print("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
+    }
+    
+    /// Tells the delegate that a full-screen view will be presented in response
+    /// to the user clicking on an ad.
+    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
+        print("adViewWillPresentScreen")
+    }
+    
+    /// Tells the delegate that the full-screen view will be dismissed.
+    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
+        print("adViewWillDismissScreen")
+    }
+    
+    /// Tells the delegate that the full-screen view has been dismissed.
+    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
+        print("adViewDidDismissScreen")
+    }
+    
+    /// Tells the delegate that a user click will open another app (such as
+    /// the App Store), backgrounding the current app.
+    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
+        print("adViewWillLeaveApplication")
+    }
+    
+}
--- iosDeKasegu/SiritoriViewController.swift
+++ iosDeKasegu/SiritoriViewController.swift
@@ -1,15 +1,15 @@
 //
-//  ViewController.swift
+//  SiritoriViewController.swift
 //  iosDeKasegu
 //
-//  Created by 高木広大 on 2018/06/29.
+//  Created by 高木広大 on 2018/07/01.
 //  Copyright © 2018年 shonanhiratsuka. All rights reserved.
 //
 
 import UIKit
 import GoogleMobileAds
 
-class ViewController: BaseViewController {
+class SiritoriViewController: BaseViewController {
     var bannerView: GADBannerView!
     
     override func viewDidLoad() {
@@ -22,13 +22,23 @@ class ViewController: BaseViewController {
         bannerView.load(GADRequest())
         bannerView.delegate = self
         addBannerViewToView(bannerView)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
