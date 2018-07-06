8f2f3a6d083589f2f753bcafd8648c5d1e928fc9
user <matsuei>
2018-07-07 03:31:18 +0900

    中央の9x9に書いた内容が周囲の9x9に反映出来るように変更

--- iosDeKasegu/MandaraWorkTableViewController.swift
+++ iosDeKasegu/MandaraWorkTableViewController.swift
@@ -9,19 +9,26 @@
 import UIKit
 import GoogleMobileAds
 
-class MandaraWorkTableViewController: BaseViewController {
+class MandaraWorkTableViewController: BaseViewController,UITextViewDelegate {
     var bannerView: GADBannerView!
     
     @IBOutlet weak var themeLabel: UILabel!
     @IBOutlet weak var waku1: UITextView!
+    @IBOutlet weak var waku2: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        waku1.delegate = self
+        waku2.delegate = self
 
         waku1.layer.borderColor = UIColor.red.cgColor
         
         // 枠の幅
         waku1.layer.borderWidth = 1.0
+    }
+    
+    func textViewDidChange(_ waku1: UITextView) {
+        waku2.text = waku1.text;
     }
 
     override func didReceiveMemoryWarning() {
