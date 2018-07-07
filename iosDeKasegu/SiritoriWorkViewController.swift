0dd5dd1f092085041240d426e42e5b8027dba5dd
Kodai Takagi <mj5824@icloud.com>
2018-07-07 10:53:38 +0900

    しりとり法作成 ・次へボタンでキーワードとアイデアを入力フォームが増えていくように実装

--- iosDeKasegu/SiritoriWorkViewController.swift
+++ iosDeKasegu/SiritoriWorkViewController.swift
@@ -12,15 +12,45 @@ import GoogleMobileAds
 class SiritoriWorkViewController: BaseViewController {
     var bannerView: GADBannerView!
     
-    @IBOutlet weak var themeLabel: UILabel!
+    // XXX: 別々の配列じゃなくて構造体とかにするべき、おそらく
+    var keywordArray:[UITextField] = []
+    var IdeaArray:[UITextView] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // siritoriのテーマを読込
-        themeLabel.backgroundColor = UIColor.white
-        themeLabel.text = "~テーマ~\n    " + siritoriTheme
+        // XXX: CGRectのパラメータを数値でなく、画面の幅の半分とかにしないと別の機器で表示が崩れる、多分
+        
+        // siritoriのテーマを読込んで表示
+        let themeLabel = UILabel()
+        themeLabel.frame = CGRect(x:50, y:50, width:300, height:80)
+        themeLabel.numberOfLines = 0
+        themeLabel.text = "★テーマ★\n　　　　" + siritoriTheme
+        themeLabel.backgroundColor = UIColor.gray
+        self.view.addSubview(themeLabel)
 
+        // キーワード入力用
+        let firstKeyword: UITextField = UITextField(frame: CGRect(x: 10, y:200, width:100, height:30))
+        firstKeyword.borderStyle = UITextBorderStyle.roundedRect
+        keywordArray.append(firstKeyword)
+        view.addSubview(firstKeyword)
+        
+        // アイデア入力用
+        let firstIdea: UITextView = UITextView(frame: CGRect(x: 150, y:200, width:200, height:60))
+        firstIdea.layer.borderWidth = 1
+        firstIdea.layer.cornerRadius = 5
+        firstIdea.layer.borderColor = UIColor.lightGray.cgColor
+        IdeaArray.append(firstIdea)
+        view.addSubview(firstIdea)
+        
+        // ボタンの追加
+        let button = UIButton()
+        button.setTitle("次へ", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        button.frame = CGRect(x: 300, y: 270, width: 50, height: 50)
+        button.addTarget(self, action: #selector(self.onClick(_:)), for: .touchUpInside)
+        view.addSubview(button)
+        
         // To display the advertisement
         bannerView = GADBannerView(adSize: kGADAdSizeBanner)
         bannerView.adUnitID = admob_id
@@ -31,10 +61,32 @@ class SiritoriWorkViewController: BaseViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func onClick(_ sender: AnyObject){
+        let button = sender as! UIButton
+        let index = keywordArray.count
+        let y_field = 200 + index * 80
+        // キーワードフィールドの追加
+        let keywordField: UITextField = UITextField(frame: CGRect(x: 10, y:y_field, width:100, height:30))
+        keywordField.borderStyle = UITextBorderStyle.roundedRect
+        keywordArray.append(keywordField)
+        view.addSubview(keywordField)
+        
+        // アイデアフィールドの追加
+        let IdeaField: UITextView = UITextView(frame: CGRect(x: 150, y:y_field, width:200, height:60))
+        IdeaField.layer.borderWidth = 1
+        IdeaField.layer.cornerRadius = 5
+        IdeaField.layer.borderColor = UIColor.lightGray.cgColor
+        IdeaArray.append(IdeaField)
+        view.addSubview(IdeaField)
+        
+        // ボタンを下げる
+        button.frame = CGRect(x:300, y:y_field+50, width:50, height:50)
     }
     
 
