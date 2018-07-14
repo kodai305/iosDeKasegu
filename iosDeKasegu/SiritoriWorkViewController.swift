b412c110f135aed980b9614c73a580c8c1f0d6ff
Kodai Takagi <mj5824@icloud.com>
2018-07-14 16:47:57 +0900

    暫定版

--- iosDeKasegu/SiritoriWorkViewController.swift
+++ iosDeKasegu/SiritoriWorkViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 import GoogleMobileAds
 
 // XXX: 別々の配列じゃなくて構造体とかにするべき、おそらく
-var keywordArray:[UITextField] = []
-var IdeaArray:[UITextView] = []
+var KeywordTextViewArray:[UITextField] = []
+var IdeaTextViewArray:[UITextView] = []
+
+// 保存用
+var IdeaDataArray:[(keyword: String, idea: String)] = []
 
 class SiritoriWorkViewController: BaseViewController {
     let firstWord = "アイデア"
@@ -26,78 +29,45 @@ class SiritoriWorkViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let index = keywordArray.count
-        
-// XXX: CGRectのパラメータを数値でなく、画面の幅の半分とかにしないと別の機器で表示が崩れる、多分
-        scrollView.backgroundColor = UIColor.gray
-        scrollView.keyboardDismissMode = .onDrag
-        // 表示窓のサイズと位置を設定
-        let scrollFrame = CGRect(x:0 , y:110 , width:view.frame.width, height:view.frame.height-20)
-        scrollView.frame = scrollFrame
-        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height-20+CGFloat(140*index))
-
+        //保存してある配列の読込
+        IdeaDataArray = readData() as! [(keyword: String, idea: String)]
+        let index = KeywordTextViewArray.count
         // しりとりのテーマを読込んで表示
-        let themeLabel = UILabel()
-        themeLabel.frame = CGRect(x:0, y:60, width:view.frame.width, height:50)
-        themeLabel.numberOfLines = 0
-        
-        themeLabel.text = "★テーマ★\n　　　　" + UserDefaults.standard.string(forKey: "SiritoriTheme")!
-        themeLabel.textColor = UIColor.white
-        themeLabel.backgroundColor = UIColor.blue
-        view.addSubview(themeLabel)
-
+        displayTheme()
         // しりとりの最初のワードを設定・表示
-        let contentsView = UIView()
-        contentsView.frame = CGRect(x:20, y:10, width:Int(view.frame.width-CGFloat(40)), height:90)
-        contentsView.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.6, alpha: 1.0)
-            // 説明
-        let firstWordLabel = UILabel()
-        firstWordLabel.frame = CGRect(x:10, y:5, width:contentsView.frame.width-20, height:30)
-        firstWordLabel.text = "しりとりの最初のフレーズ"
-        firstWordLabel.textColor = UIColor.black
-        contentsView.addSubview(firstWordLabel)
-            // 最初のワード
-        let firstWordField = UITextField(frame: CGRect(x: 10, y:40, width:140, height:30))
-        firstWordField.borderStyle = UITextBorderStyle.roundedRect
-        firstWordField.text = firstWord
-        contentsView.addSubview(firstWordField)
-
-        scrollView.addSubview(contentsView)
-        
+        createInitialWord()
         // キーワード+アイデアのコンテンツを作成, すでにあれば表示だけする
         if (index == 0) {
             createContentsView(ArrayIndex: 0)
         } else {
             for i in 0..<index {
                 loadContentsView(ArrayIndex: i)
+                IdeaDataArray = readData() as! [(keyword: String, idea: String)]
             }
         }
-        
         // ボタンの追加
         addNextButton()
-        
-        // スクロールビューをビューに追加
+        // スクロールビューを設定・追加
+        scrollView.backgroundColor = UIColor.gray
+        scrollView.keyboardDismissMode = .onDrag
+        scrollView.frame = CGRect(x:0 , y:110 , width:view.frame.width, height:view.frame.height-20)
+        scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height-20+CGFloat(140*index))
         self.view.addSubview(scrollView)
-        
         // 広告の表示
         displayAdvertisement()
-        
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
+    // 「次へ」ボタンが押されたときの処理
     @objc func onClick(_ sender: AnyObject){
         let button = sender as! UIButton
-        let index = keywordArray.count
+        let index = KeywordTextViewArray.count
         let y_new = index*140
         let y_field = 120 + y_new
         
         // キーワードが入力されていなかったらアラートを出す
-        if (keywordArray[index-1].text?.isEmpty)! {
-            print("hoge")
+        print("index:")
+        print(index)
+        if (KeywordTextViewArray[index-1].text?.isEmpty)! {
             let alert = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
             alert.title = "Ops!!"
             alert.message = "please input a keyword!"
@@ -109,7 +79,6 @@ class SiritoriWorkViewController: BaseViewController {
             self.present(alert, animated: true)
             return
         }
-        
         // キーワード+アイデアを作成
         createContentsView(ArrayIndex: Int(index))
         // ボタンを下げる
@@ -118,6 +87,21 @@ class SiritoriWorkViewController: BaseViewController {
         scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height + CGFloat(y_new))
         // 中心を変更する
         scrollView.setContentOffset(CGPoint(x: 0, y: y_field-200), animated: true)
+        // 配列に追加
+        var tuple_keyword:String = "-"
+        if (KeywordTextViewArray[index-1].text?.isEmpty)! {
+            tuple_keyword = KeywordTextViewArray[index-1].text! as String
+        }
+        var tuple_idea:String = "-"
+//        if (IdeaTextViewArray[index-1].text?.isEmpty)! {
+//            tuple_idea = IdeaTextViewArray[index-1].text!
+//        }
+        IdeaDataArray.append((tuple_keyword, tuple_idea))
+        print("IdeaData index:")
+        print(IdeaDataArray.count)
+        // 保存
+//        saveData(ideaArray: IdeaDataArray)
+        saveData()
     }
     
     
@@ -137,9 +121,12 @@ class SiritoriWorkViewController: BaseViewController {
         keywordLabel.text = "キーワード"+String(ArrayIndex+1)
         keywordLabel.textColor = UIColor.black
         contentsView.addSubview(keywordLabel)
-        
-        // キーワードフィールドの追加
-        contentsView.addSubview(keywordArray[ArrayIndex])
+        // キーワードフィールドの作成・追加
+        let keywordField = UITextField(frame: CGRect(x: 10, y:40, width:140, height:30))
+        keywordField.borderStyle = UITextBorderStyle.roundedRect
+        keywordField.text = IdeaDataArray[ArrayIndex].keyword
+        KeywordTextViewArray.append(keywordField)
+        contentsView.addSubview(keywordField)
         
         // アイデアのラベルを追加
         let IdeaLabel = UILabel()
@@ -148,10 +135,14 @@ class SiritoriWorkViewController: BaseViewController {
         IdeaLabel.text = "アイデア"+String(ArrayIndex+1)
         IdeaLabel.textColor = UIColor.black
         contentsView.addSubview(IdeaLabel)
-        
-        // アイデアフィールドの追加
-        contentsView.addSubview(IdeaArray[ArrayIndex])
-        
+        // アイデアフィールドの作成・追加
+        let IdeaView: UITextView = UITextView(frame: CGRect(x: 160, y:40, width:160, height:60))
+        IdeaView.layer.borderWidth = 1
+        IdeaView.layer.cornerRadius = 5
+        IdeaView.layer.borderColor = UIColor.lightGray.cgColor
+        IdeaTextViewArray.append(IdeaView)
+        contentsView.addSubview(IdeaView)
+
         // スクロールビューに追加
         scrollView.addSubview(contentsView)
     }
@@ -177,12 +168,12 @@ class SiritoriWorkViewController: BaseViewController {
         let keywordField: UITextField = UITextField(frame: CGRect(x: 10, y:40, width:140, height:30))
         keywordField.delegate = self as? UITextFieldDelegate
         keywordField.borderStyle = UITextBorderStyle.roundedRect
-        keywordArray.append(keywordField)
         if (ArrayIndex == 0) {
             keywordField.placeholder = firstWord+"→ ..."
         } else {
-            keywordField.placeholder = keywordArray[ArrayIndex-1].text!+"→ ..."
+            keywordField.placeholder = KeywordTextViewArray[ArrayIndex-1].text!+"→ ..."
         }
+        KeywordTextViewArray.append(keywordField)
         contentsView.addSubview(keywordField)
         
         // アイデアのラベルを追加
@@ -193,20 +184,20 @@ class SiritoriWorkViewController: BaseViewController {
         IdeaLabel.textColor = UIColor.black
         contentsView.addSubview(IdeaLabel)
         
-        // アイデアフィールドの追加
-        let IdeaField: UITextView = UITextView(frame: CGRect(x: 160, y:40, width:160, height:60))
-        IdeaField.layer.borderWidth = 1
-        IdeaField.layer.cornerRadius = 5
-        IdeaField.layer.borderColor = UIColor.lightGray.cgColor
-        IdeaArray.append(IdeaField)
-        contentsView.addSubview(IdeaField)
+        // アイデアビューの追加
+        let IdeaView: UITextView = UITextView(frame: CGRect(x: 160, y:40, width:160, height:60))
+        IdeaView.layer.borderWidth = 1
+        IdeaView.layer.cornerRadius = 5
+        IdeaView.layer.borderColor = UIColor.lightGray.cgColor
+        IdeaTextViewArray.append(IdeaView)
+        contentsView.addSubview(IdeaView)
         
         // スクロールビューに追加
         scrollView.addSubview(contentsView)
     }
     
     func addNextButton() {
-        var index = keywordArray.count // indexの更新
+        let index = IdeaDataArray.count // indexの更新
         let button = UIButton()
         button.backgroundColor = UIColor.white
         button.layer.borderWidth = 2.0 // 枠線の幅
@@ -229,10 +220,70 @@ class SiritoriWorkViewController: BaseViewController {
     }
     
     public func resetContents() {
-        keywordArray.removeAll()
-        IdeaArray.removeAll()
+        IdeaDataArray.removeAll()
+        IdeaDataArray.removeAll()
     }
 
+//    func saveData(_: ideaArray) {
+    func saveData() {
+        let texts: [String] = ["三日月宗近"]
+        
+        let defaults = UserDefaults.standard
+//        defaults.set(IdeaDataArray ,forKey: "Cell_1_data")
+        defaults.set(texts ,forKey: "Cell_1_data")
+    }
+    
+    func readData() -> ([(Any, Any)]) {
+        let defaults = UserDefaults.standard
+        let stubArray:[(Any, Any)] = []
+        if let ideaArray = defaults.array(forKey: "Cell_1_data") {
+            return ideaArray as! ([(Any, Any)])
+        } else {
+            return stubArray
+        }
+
+    }
+
+    func readTheme() -> (String) {
+        let defaults = UserDefaults.standard
+        if let theme:String = defaults.string(forKey: "Cell_1_theme") {
+            return theme
+        } else {
+            return "1"
+        }
+    }
+    
+    func createInitialWord() {
+        let contentsView = UIView()
+        contentsView.frame = CGRect(x:20, y:10, width:Int(view.frame.width-CGFloat(40)), height:90)
+        contentsView.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.6, alpha: 1.0)
+        let firstWordLabel = UILabel()
+        firstWordLabel.frame = CGRect(x:10, y:5, width:contentsView.frame.width-20, height:30)
+        firstWordLabel.text = "しりとりの最初のフレーズ"
+        firstWordLabel.textColor = UIColor.black
+        contentsView.addSubview(firstWordLabel)
+        let firstWordField = UITextField(frame: CGRect(x: 10, y:40, width:140, height:30))
+        firstWordField.borderStyle = UITextBorderStyle.roundedRect
+        firstWordField.text = firstWord
+        contentsView.addSubview(firstWordField)
+        scrollView.addSubview(contentsView)
+    }
+    
+    func displayTheme() {
+        let themeLabel = UILabel()
+        themeLabel.frame = CGRect(x:0, y:60, width:view.frame.width, height:50)
+        themeLabel.numberOfLines = 0
+        themeLabel.text = "★テーマ★\n　　　　" + readTheme()
+        themeLabel.textColor = UIColor.white
+        themeLabel.backgroundColor = UIColor.blue
+        view.addSubview(themeLabel)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
     /*
     // MARK: - Navigation
 
