1a06670dd7a21e4c84d853650da1ec0f089c61ef
user <matsuei>
2018-07-25 23:41:40 +0900

    しりとりのチュートリアルを作成（暫定版）

--- iosDeKasegu/SiritoriGuideViewController.swift
+++ iosDeKasegu/SiritoriGuideViewController.swift
@@ -12,34 +12,67 @@ class SiritoriGuideViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let datum_x:CGFloat = UIScreen.main.bounds.width*0.1
-        let datum_y:CGFloat = 0.0
-        let stand_w:CGFloat = UIScreen.main.bounds.width*0.8
-        let stand_h:CGFloat = UIScreen.main.bounds.height*0.8
+        let Datum_xOfScrollView:CGFloat = UIScreen.main.bounds.width*0.1
+        let Datum_yOfScrollView:CGFloat = UIScreen.main.bounds.height*0.1
+        let WidthOfScrollView:CGFloat = UIScreen.main.bounds.width*0.8
+        let HeightOfScrollView:CGFloat = UIScreen.main.bounds.height*0.9
         
         
         let scrollView = UIScrollView()
-        scrollView.frame = CGRect(x: datum_x, y: datum_y, width: stand_w, height: stand_h)
-        scrollView.contentSize = CGSize(width: stand_w * 3.0, height: stand_h)
+        scrollView.frame = CGRect(x: Datum_xOfScrollView, y: Datum_yOfScrollView, width: WidthOfScrollView, height: HeightOfScrollView)
+        scrollView.contentSize = CGSize(width: WidthOfScrollView * 3.0, height: HeightOfScrollView)
         scrollView.isPagingEnabled = true
         
         // 1枚目の画像
-        let firstImageView = UIImageView(image: UIImage(named: "expand.png"))
-        firstImageView.frame = CGRect(x: 0, y: 0, width: stand_w, height: stand_h)
-        firstImageView.contentMode = UIViewContentMode.scaleAspectFit
-        scrollView.addSubview(firstImageView)
+        let FirstUIView = UIView(frame: CGRect(x: 0, y: 0, width: WidthOfScrollView, height: HeightOfScrollView))
+        let FirstImage = UIImageView(image: UIImage(named: "SiritoriTutorialImage1.png"))
+        //FirstImage.frame.size = CGSize(width: WidthOfScrollView, height: HeightOfScrollView * 0.8)
+        FirstImage.frame.size.height = HeightOfScrollView * 0.8
+        FirstImage.center = CGPoint(x: WidthOfScrollView * 0.5, y: HeightOfScrollView * 0.4)
+        FirstImage.contentMode = UIViewContentMode.scaleAspectFit
         
+        let FirstLable = UILabel()
+        FirstLable.text = "新規テーマ:画面右上の＋ボタン \n 保存テーマ:テーブルの項目 \n をそれぞれタップ"
+        FirstLable.numberOfLines = 5
+        FirstLable.font = UIFont.systemFont(ofSize: 20.0)
+        FirstLable.frame = CGRect(x:0, y: HeightOfScrollView * 0.72,width: WidthOfScrollView, height: HeightOfScrollView * 0.3)
+        FirstLable.adjustsFontSizeToFitWidth = true
+        FirstUIView.addSubview(FirstImage)
+        FirstUIView.addSubview(FirstLable)
+        scrollView.addSubview(FirstUIView)
         // 2枚目の画像
-        let secondImageView = UIImageView(image: UIImage(named: "getidea.png"))
-        secondImageView.frame = CGRect(x: stand_w, y: 0, width: stand_w, height: stand_h)
-        secondImageView.contentMode = UIViewContentMode.scaleAspectFit
-        scrollView.addSubview(secondImageView)
+        let SecondUIView = UIView(frame: CGRect(x: WidthOfScrollView, y: 0, width: WidthOfScrollView, height: HeightOfScrollView))
+        let SecondImage = UIImageView(image: UIImage(named: "SiritoriTutorialImage2.png"))
+        SecondImage.frame.size.height = HeightOfScrollView * 0.8
+        SecondImage.center = CGPoint(x: WidthOfScrollView * 0.5, y: HeightOfScrollView * 0.4)
+        SecondImage.contentMode = UIViewContentMode.scaleAspectFit
+        
+        let SecondLable = UILabel()
+        SecondLable.text = "最初のフレーズを決定 \n （デフォルト:しりとり）\n 最初のフレーズとしりとりをして \n キーワード1を決定"
+        SecondLable.numberOfLines = 5
+        SecondLable.font = UIFont.systemFont(ofSize: 20.0)
+        SecondLable.frame = CGRect(x:0, y: HeightOfScrollView * 0.72,width: WidthOfScrollView, height: HeightOfScrollView * 0.3)
+        SecondLable.adjustsFontSizeToFitWidth = true
+        SecondUIView.addSubview(SecondImage)
+        SecondUIView.addSubview(SecondLable)
+        scrollView.addSubview(SecondUIView)
         
         // 3枚目の画像
-        let thirdImageView = UIImageView(image: UIImage(named: "tutorial.png"))
-        thirdImageView.frame = CGRect(x: stand_w * 2.0, y: 0, width: stand_w, height: stand_h)
-        thirdImageView.contentMode = UIViewContentMode.scaleAspectFit
-        scrollView.addSubview(thirdImageView)
+        let ThirdUIView = UIView(frame: CGRect(x: WidthOfScrollView * 2, y: 0, width: WidthOfScrollView, height: HeightOfScrollView))
+        let ThirdImage = UIImageView(image: UIImage(named: "SiritoriTutorialImage3.png"))
+        ThirdImage.frame.size.height = HeightOfScrollView * 0.8
+        ThirdImage.center = CGPoint(x: WidthOfScrollView * 0.5, y: HeightOfScrollView * 0.4)
+        ThirdImage.contentMode = UIViewContentMode.scaleAspectFit
+        
+        let ThirdLable = UILabel()
+        ThirdLable.text = "キーワード1を含むアイデア1を考案 \n 次へボタンをタップして \n キーワード2のカードを作成 \n 100個のアイデアを目指そう！"
+        ThirdLable.numberOfLines = 5
+        ThirdLable.font = UIFont.systemFont(ofSize: 20.0)
+        ThirdLable.frame = CGRect(x:0, y: HeightOfScrollView * 0.72,width: WidthOfScrollView, height: HeightOfScrollView * 0.3)
+        ThirdLable.adjustsFontSizeToFitWidth = true
+        ThirdUIView.addSubview(ThirdImage)
+        ThirdUIView.addSubview(ThirdLable)
+        scrollView.addSubview(ThirdUIView)
         
         // スクロールビューを追加
         self.view.addSubview(scrollView)
