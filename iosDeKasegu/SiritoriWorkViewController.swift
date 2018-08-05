79a8d4ed2ef0135855b895204c52857a30f5a1d1
Kodai Takagi <mj5824@icloud.com>
2018-08-05 10:52:54 +0900

    しりとりでscrollviewが小さすぎて新規カードを追加できなくなることがあるバグを修正

--- iosDeKasegu/SiritoriWorkViewController.swift
+++ iosDeKasegu/SiritoriWorkViewController.swift
@@ -75,7 +75,7 @@ class SiritoriWorkViewController: BaseWorkViewController, UITextFieldDelegate {
         //背景色（PORCELAIN）
         scrollView.backgroundColor = UIColor(hex: "ECF0F1", alpha: 10.0)
         scrollView.keyboardDismissMode = .onDrag
-        scrollView.frame = CGRect(x:0 , y:60 + view.frame.height / 10, width:view.frame.width, height:view.frame.height-20)
+        scrollView.frame = CGRect(x:0 , y:60 + view.frame.height / 10, width:view.frame.width, height:view.frame.height-50)
         scrollView.contentSize = CGSize(width: view.frame.width, height: view.frame.height-20+CGFloat(140*index))
         self.view.addSubview(scrollView)
         
